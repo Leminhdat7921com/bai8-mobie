445d53770ff89817f99c0f6be45d5c7d7b365ef8
Leminhdat7921com <105043619+Leminhdat7921com@users.noreply.github.com>
2025-03-04 10:01:19 +0700

    App.js

--- App.js
+++ App.js
@@ -1,1 +1,21 @@
 
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './screens/LoginScreen';
+import ExplorerScreen from './screens/ExplorerScreen';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="LoginScreen">
+                <Stack.Screen name="LoginScreen" component={LoginScreen} options={{ headerShown: false }} />
+                <Stack.Screen name="ExplorerScreen" component={ExplorerScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default App;
